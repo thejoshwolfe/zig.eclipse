ff278e5ff62f3ba0e1d53ed10d6e8dc83561bbc2
Josh Wolfe <thejoshwolfe@gmail.com>
2018-06-28 01:11:36 -0400

    a dummy wizard that creates a project

---
+++ workspace/org.ziglang.eclipse/src/org/ziglang/eclipse/NewZigProjectWizard.java
@@ -0,0 +1,8 @@
+package org.ziglang.eclipse;
+
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
+
+public class NewZigProjectWizard extends BasicNewProjectResourceWizard implements INewWizard
+{
+}
