abdde7574d57bb41ac399ebbd5c82dc6e0312943
Josh Wolfe <thejoshwolfe@gmail.com>
2018-06-29 22:17:12 -0400

    builder works, but is completely silent

--- workspace/org.ziglang.eclipse/src/org/ziglang/eclipse/ZigProjectBuilder.java
+++ workspace/org.ziglang.eclipse/src/org/ziglang/eclipse/ZigProjectBuilder.java
@@ -1,8 +1,10 @@
 package org.ziglang.eclipse;
 
+import java.io.IOException;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -12,7 +14,23 @@ public class ZigProjectBuilder extends IncrementalProjectBuilder
     @Override
     protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor) throws CoreException
     {
-        System.out.println("building...?");
+        ProcessBuilder processBuilder = new ProcessBuilder("zig", "build", "--build-file", "build.zig");
+        processBuilder.directory(getProject().getLocation().toFile());
+        Process process;
+        try {
+            process = processBuilder.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            process.waitFor();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
+
         return null;
     }
 }
